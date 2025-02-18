3ff7422ea1b8ccb6a6ec4348d3c175810de4827a
dharanr <rdharan7596@gmail.com>
2025-02-18 03:03:44 -0500

    POC on the DeepSeek r1 1.5b, involving the prompts to simplify the JD

--- main.py
+++ main.py
@@ -1,7 +1,90 @@
 from fastapi import FastAPI
+import ollama
+
+from services import DBService, LLMService
 
 app = FastAPI()
 
+import os
+os.environ["OLLAMA_CUDA"] = "1"
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    jd ="""About the job
+            As a Software Developer at Bucketlist Rewards, you will help create the next generation of our rewards and recognition program. You will work collaboratively with a cross-functional agile team and gain experience with a wide variety of tasks. We value clean pragmatic solutions, sustainable growth, and provide opportunities based on your interests. If you're passionate about software development and would like to join a diverse and experienced team, we're looking for someone like you!
+
+
+
+            About Us: Bucketlist Rewards provides a leading B2B SaaS solution that allows organizations of all sizes to introduce effective rewards and recognition programs to its workforce. Our goal is to provide companies with an easy-to-use web platform that enables employers to reward team members in a meaningful way that matters to them, while achieving great business results and building better organizational cultures. When it comes to great company culture, the Bucketlist team walks the talk and we are proud to be Great Place to Work certified!
+
+
+
+            Key Responsibilities:
+
+            Programming in Python and Django, Angular, and React.
+            Working frequently with Linux, PostgreSQL, Kubernetes, Docker, and cloud services (AWS).
+            Learning best practices in regards to design, testing and automation.
+            Participating in agile ceremonies and other collaborative team activities.
+            Involvement with DevOps, QA, Support as well as product development.
+
+
+            Required Skills for Success:
+
+            At least 3 years of full stack development experience.
+            At least 3 years of Object-oriented programming.
+            BSc in Computer Science, Software Engineering or equivalent.
+            Positive attitude and a desire to learn.
+            Excellent problem solving skills.
+            Comfort working independently and with a remote team.
+            Passion for software design using recognized best practices.
+            Basic Linux command-line literacy.
+            Excellent English communication skills.
+            Familiarity with git and project tracking software such as Jira.
+
+
+            Nice to have:
+
+            Experience programming with Python and Django.
+            Experience programming with front-end frameworks such as React.
+            Familiarity with design patterns and testing techniques.
+            Familiarity with containers and cloud computing.
+            Familiarity with CI/CD principles.
+
+
+            Why Join Us?
+
+            At Bucketlist, we believe in meaningful work and meaningful rewards. You’ll join a supportive, collaborative team that values your growth and provides opportunities to explore your interests. Together, we’re building a platform that helps organizations create better workplaces, one recognition at a time.
+
+
+
+            The Perks:
+
+            Growth Opportunities: Join a growing company where you’ll be part of a high-performing team with lots of potential.
+            Remote Work: We are a distributed workplace (100% remote). This position is open to anyone located in a timezone that aligns with continental North America.
+            Wellness Days: In addition to paid vacation days, we offer flexible paid time off to fit individual needs.
+            Comprehensive Benefits Package: Includes health, dental, vision care, EFAP, and more.
+            Annual Bucketlist Benefit: An additional paid day off and $$ to spend to help check items off your own bucket list!
+            Rewards and Recognition: We drink our own champagne! We’ll help you achieve not just your career goals but your personal goals as well!
+
+
+            Anticipated Salary Range: The base salary for this role is $80-110k CAD annually for candidates located in Canada. The final offer will consider factors such as your expertise and previous experience. For candidates located outside of Canada, the annual salary will be adjusted based on the local market expectations.
+
+
+
+            At Bucketlist Rewards, we are committed to fostering a diverse, equitable, and inclusive workplace. We believe that varied perspectives drive innovation and creativity, and we welcome applicants from all backgrounds and experiences. We encourage individuals from underrepresented groups to apply, as we strive to build a team that reflects the diverse communities we serve. We are dedicated to creating an environment where everyone feels valued, respected, and empowered to contribute their best work."""
+    
+    
+
+
+    # res = DBService('resumes').queryDocuments([jd], 20)
+    # docs = res["documents"][0]
+    # evaluations = []
+    # for i in range(5):
+    #     from_index = i
+    #     to_index = (i*5)+i
+    #     evaluations.append(LLMService().Scoring_LLMQuery(jd, docs[from_index:to_index]))
+
+    # scores = LLMService().Scoring_LLMQuery(jd, docs[0]) # 3 resumes
+    # scores  = ollama.generate(model="mistral", prompt="hii")
+    return LLMService().insights_query(jd)
--- services.py
+++ services.py
@@ -1,5 +1,6 @@
 import chromadb
 import PyPDF2
+import ollama
 
 chroma_client = chromadb.PersistentClient("data/")
 
@@ -10,6 +11,13 @@ class DBService:
     def addDocuments(self, documents, metadatas, ids):
         # For now the document and the embeddings are taken care by chroma
         self.collection.add(documents=documents, metadatas=metadatas, ids=ids)
+
+    def getDocuments(self, ids):
+        newids = []
+        for id in ids:
+            newids.append(str(id))
+        print("-----------", newids)
+        return self.collection.get(ids=newids, include=['documents', 'metadatas'])
 
     def queryDocuments(self, query_texts, topk):
         return self.collection.query(query_texts=query_texts, n_results=topk)
@@ -35,4 +43,69 @@ class PDFService:
             return None
         
 class LLMService:
-    pass
+    def _evaluation_prompt(cls, job_description, resume):
+        return f"""
+            Given Job description and resumes
+            
+            ### Job Description:
+            ----------------------------------------------------------------------------------------
+            {job_description}
+            ----------------------------------------------------------------------------------------
+
+            ### Resume:
+            ----------------------------------------------------------------------------------------
+            {resume}
+            ----------------------------------------------------------------------------------------
+
+            Now Imagine Yourself that you are an expert resume evaluator. 
+            Your job is to evaluate and score the resume based on how well they fit the given job description.
+
+            Below are the key compontents that you need to evaluate - the score on each should be between 0 and 10:
+            1. Skill Match (SM)
+            2. Years of Experience (YOE)
+            3. Industry Relevance (IR)
+            4. Education Fit (EF)
+            5. Recent Experience (RE)
+
+            And return the JSON in the below format: the value should be score alone.
+            Only JSON is accepted, no other explanations are needed anywhere in the response.
+            {
+                {"SM": "x","YOE": "y","IR": "z","EF": "u","RE": "v"}
+            }
+            """
+    
+    def jobdescription_insights(cls, job_description):
+        return f"""
+            Here is the job description:
+            {job_description}
+            ----------------------------------------------------------------------------------------
+
+            Given the job description, cut down the unnecessary information and shrink the job description as per the below headings:
+            1. Educational qualifications required - like the degree,schooling, diplomas (purely academic qualifications)
+            2. Years of experience required - minimum how many experience is required and how far they are tolerable
+            3. Hard Skills Required - Technologies, tools, languages, framework, etc
+            4. Soft Skills Required - communication, leadership, collaboration, etc
+            5. Key responsibilities - what the candidate is expected to do on their day to day job, how well are they expected to perform
+            6. Nice to have skills - the skills that are not mandatory but good to have
+
+            In all the above headings, the values should be extracted from the given job description, and it should be the same keywords and no modifications allowed.
+            It should be descriptive and not subjective. 
+
+            Things to note:
+            This data will be used to evaluate the resumes and score them based on how well they match the job description.
+            So make sure you extract the right information and make it as clear as possible.
+            Need 100% accuracy and no mistakes are allowed. The information you are going to give must reflect the job description accurately and entirely.
+
+            And most importantly - the total character count should not exceed 1500 characters and should not be less than 1200.
+            """
+    
+    def insights_query(cls, job_description):
+        return ollama.chat(model="deepseek-r1:1.5b", messages=[{"role": "assistant", "content": cls.jobdescription_insights(job_description), "stream": False}])
+    
+    def Scoring_LLMQuery(cls, job_description, resume):
+        prompt = cls._evaluation_prompt(job_description, resume)
+        print(prompt)
+        return ollama.chat(model="deepseek-r1:1.5b", messages=[{"role": "assistant", "content": prompt, "stream": False , "options":{"max_tokens": 50}}])
+        # return ollama.generate(model="deepseek-r1:1.5b", prompt=cls._evaluation_prompt(job_description, resume))
+            # No more explanations are needed. Just the scores for each resume. More like a JSON array of objects.
+            # Total score should be between 0-50 ( which you have to just sum up the above 5 scores)
